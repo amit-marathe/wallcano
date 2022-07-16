4e6bafaba2136765de4f15bebe632f313dc03b7a
Amit Marathe <95067232+AmitMarathe9098@users.noreply.github.com>
2022-07-16 12:23:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// let x = 25;
+// documents.write(x);
+
+//function fun() {  
+    // prompt ("This is a prompt box", "Hello world");  
+    
+   // var n =  Number(prompt("Enter any Number",""))
+    // if(n%2==0){
+    //     document.write("number is even")
+    // }
+    // else
+    // document.write("number is odd")
+    
+//     if(n==1)
+//     {
+//         alert("Sunday")
+//     }
+//     else if(n==2)
+//     {
+//         alert("Monday")
+//     }
+//     else if(n==3)
+//     {
+//         alert("Tuesday")
+//     }
+//     else if(n==4)
+//     {
+//         alert("Wednesday")
+//     }
+//     else if(n==5)
+//     {
+//         alert("Thuesday")
+//     }
+//     else if(n==6)
+//     {
+//         alert("Friday")
+//     }
+//     else if(n==7)
+//     {
+//         alert("Saturday")
+//     }
+//     else{
+//         alert("You Entered Wrong Number, Number should be between 1-7")
+//     }
+// }  
